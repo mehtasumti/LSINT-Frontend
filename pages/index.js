ae5785f65044b69fa97e415091ce105deefadc04
Sumti Mehta <sumti.mehta.01@gmail.com>
2026-01-10 14:45:42 +0530

    Testing GitHub Actions Sumti - Test 2

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,10 @@ export default function Home() {
         <div className={styles.featureCard}>
           <h3>GitHub Actions Sumti</h3>
           <p>Testing this for GitHub Actions for Interview Test</p>
+        <div className={styles.featureCard}>
+          <h3>Git Action</h3>
+          <p>Testing this for GitHub Actions for Interview Test</p>
+        </div>
         </div>
       </div>
     </div>
